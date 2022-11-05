c4074a8607648f7dcff8f51357fe5be84381b693
Dang-Xuan-Dung <dungdx156@gmail.com>
2022-11-06 01:08:31 +0700

    upload source code

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const searchBtn = document.querySelector(".search-btn");
+const searchBox = document.querySelector(".search-box");
+
+
+searchBtn.addEventListener("click", ()=>{
+  searchBox.classList.toggle("show")
+})
